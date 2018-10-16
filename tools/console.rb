55f9596b16287fe0f7373d6d6a182f97c8cf5833
CarsonRey <c.rey494@gmail.com>
2018-10-16 17:34:29 -0400

    practiced Has many objects through with native error messages; wrote methods for returning info about betterlyft drivers, passengers & rides

--- tools/console.rb
+++ tools/console.rb
@@ -4,7 +4,24 @@ def reload
   load 'config/environment.rb'
 end
 
+ali = Passenger.new("Ali")
+carson = Passenger.new("Carson")
+ron = Passenger.new("Ron")
+nina = Driver.new("Nina")
+ali = Driver.new("Ali")
+
+ride1 = Ride.new(nina, carson, 5.2)
+ride2 = Ride.new(nina, carson, 5.9)
+ride3 = Ride.new(nina, ron, 101)
+ride4 = Ride.new(ali, nina, 4.2)
 
 # Put your variables here~!
 
+#nina.passenger_names
+
+#Ride.average_distance
 binding.pry
+
+
+
+# binding.pry
